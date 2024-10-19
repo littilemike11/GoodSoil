4fc595b601ba92d60e7807227c50a3e5ecc9c396
Michael <michaeledquilan@gmail.com>
2024-10-19 01:32:28 -0400

    start working on same brain

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,20 +3,19 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className="card bg-base-100 w-96 shadow-xl">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+      <h1>Same Brain</h1>
+      <p>Round: # </p>
+      <p>think of a word</p>
+
+      <p>countdown timer here</p>
+
+      <input
+      className='input px-4'
+      type="text" 
+      placeholder='RandomWord'/>
+
+      <button className='btn'>submit</button>
+
     </>
   )
 }
