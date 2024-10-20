a0a15f0c76bf73c524156ea54a12bf0a56cf4e3d
Michael <micaheledquilan@gmail.com>
2024-10-20 18:13:19 -0400

    created routes and basic landing pg and footer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import LandingPage from './pages/LandingPage.jsx'
+
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route
+}
+  from "react-router-dom"
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<LandingPage />}></Route>
+    </Route>
+
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
